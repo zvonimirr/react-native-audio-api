2355c1a439827bb39d754f46e76b5f5c93298126
Michał Dydek <dydmichal@gmail.com>
2026-05-13 16:05:43 +0200

    chore: added clang-format-ignore-file (#1069)

--- packages/react-native-audio-api/scripts/cpplint.sh
+++ packages/react-native-audio-api/scripts/cpplint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if which cpplint >/dev/null; then
-  find common/cpp android/src/main/cpp -path 'common/cpp/audioapi/libs' -prune -o -path 'common/cpp/audioapi/external' -prune -o -path 'common/cpp/audioapi/dsp/r8brain' -prune -o -path 'common/cpp/test/build' -prune -o \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) -print | xargs cpplint --linelength=100 --filter=-legal/copyright,-readability/todo,-readability/nolint,-build/namespaces,-build/include_order,-whitespace,-build/c++17,-build/c++20,-runtime/references,-runtime/string,-readability/braces --quiet --recursive "$@"
+  find common/cpp android/src/main/cpp \( -path 'common/cpp/audioapi/libs' -prune -o -path 'common/cpp/audioapi/external' -prune -o -path 'common/cpp/audioapi/dsp/r8brain' -prune -o -type d -name build -prune -o \( \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) -a -type f \) -print \) | xargs cpplint --linelength=100 --filter=-legal/copyright,-readability/todo,-readability/nolint,-build/namespaces,-build/include_order,-whitespace,-build/c++17,-build/c++20,-runtime/references,-runtime/string,-readability/braces --quiet --recursive "$@"
 else
   echo "error: cpplint not installed, download from https://github.com/cpplint/cpplint" 1>&2
   exit 1
